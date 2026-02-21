4d7c6a94557b1a87921065eeb4cbb28f1634d4d2
lmeller-git <louis.meller@icloud.com>
2026-02-21 15:09:39 +0100

    add week1 solution. add utils for running rmarkdown. add .gitignore

--- week01/install.R
+++ week01/install.R
@@ -1,12 +1,14 @@
-install.packages(c("gsl","ggplot2","remotes","rmarkdown"))
+install.packages(c("gsl", "ggplot2", "remotes", "rmarkdown"))
 # If you haven't install Bioconductor yet:
-# install.packages("BiocManager")
-# BiocManager::install()
+install.packages("BiocManager")
+BiocManager::install()
 
-BiocManager::install(c("GenomicRanges", "rtracklayer", "EnrichedHeatmap", "AnnotationHub", 
-                       "ensembldb", "edgeR", "esATAC", "sechm", "motifmatchr","rGREAT",
-                       "bsseq","DMRcate","data.table","InteractionSet","limma",
-                       "universalmotif", "MotifDb", "TFBSTools", "Biostrings", "PWMEnrich",
-                       "Rsubread","Rfastp"))
+BiocManager::install(c(
+  "GenomicRanges", "rtracklayer", "EnrichedHeatmap", "AnnotationHub",
+  "ensembldb", "edgeR", "esATAC", "sechm", "motifmatchr", "rGREAT",
+  "bsseq", "DMRcate", "data.table", "InteractionSet", "limma",
+  "universalmotif", "MotifDb", "TFBSTools", "Biostrings", "PWMEnrich",
+  "Rsubread", "Rfastp"
+))
 BiocManager::install(c("Bioconductor/BiocFileCache", "chromVAR"))
-BiocManager::install("ETHZ-INS/epiwraps", ref="devel")
+BiocManager::install("ETHZ-INS/epiwraps", ref = "devel")
